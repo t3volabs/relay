6f0f43f3ae48a8e77fde15d4dd082eb959e6836a
besoeasy <besoeasy@users.noreply.github.com>
2025-02-27 20:06:24 +0530

    stable

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 7303;
 
 const fileLimit = "10mb";
 
-// add cors support
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST");
